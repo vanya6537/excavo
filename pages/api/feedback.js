1270662251e07ad7ddfe85653204209e464de771
Netsl <vanya6537@gmail.com>
2020-06-19 18:07:36 +0400

    Simplified

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -6,7 +6,9 @@ function sendMessage(token, chat_id, text) {
     const baseUrl = `https://api.telegram.org/bot${token}/sendMessage`
     const data = {chat_id, text}
     console.log(data)
-    axios.get(baseUrl, data)
+    axios
+        .get(baseUrl, data)
+        .then(resp => console.log(resp.data))
 }
 
 export default (request, response) => {
