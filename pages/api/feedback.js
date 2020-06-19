6178614669668830d8e2dc56876ddf27cfbe3708
Netsl <vanya6537@gmail.com>
2020-06-19 18:00:46 +0400

    Logs improved

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -4,16 +4,19 @@ const telegramUserId = process.env.TELEGRAM_USER_ID
 
 function sendMessage(token, chat_id, text) {
     const baseUrl = `https://api.telegram.org/bot${token}/sendMessage`
+    const data = {chat_id, text}
+    console.log(data)
     axios
-        .get(baseUrl, {chat_id, text})
-        .then(resp => console.log(resp.body))
+        .get(baseUrl, data)
+        .then(resp => console.log(resp))
         .catch(e => console.error(e))
 }
 
 export default (request, response) => {
     const {
-        body
+        body = {chat_id: telegramUserId, text: "Hey there! Simple response!"}
     } = request
+    console.log(body)
     const messageText = JSON.stringify(body)
     sendMessage(token, telegramUserId, messageText)
     response.status(200).send(`Thank you for your feedback!`)
