e37dcad5bbe6f5416a43ec9d89ec74205af944d7
Netsl <vanya6537@gmail.com>
2020-06-19 18:44:28 +0400

    Promised

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -6,21 +6,27 @@ function sendMessage(token, chat_id, text) {
     const baseUrl = `https://api.telegram.org/bot${token}/sendMessage`
     const params = {chat_id: parseInt(chat_id), text}
     console.log(params)
-    axios
-        .get(baseUrl, {params})
-        .then(resp => {
-            console.log(resp.data)
-        })
-        .catch(e => {
-            console.error('Telegram error', e.response.data)
-        });
+    return new Promise(resolve => {
+            axios
+                .get(baseUrl, {params})
+                .then(resp => {
+                    console.log(resp.data)
+                    resolve()
+                })
+                .catch(e => {
+                    console.error('Telegram error', e.response.data)
+                    resolve()
+                })
+        }
+    )
 }
 
 module.exports = (request, response) => {
     const {
         note = "Hey there! Simple response!", emotion = null
     } = request.body
-    const messageText = JSON.stringify({note, emotion})
-    sendMessage(token, telegramUserId, messageText)
-    response.status(200).send(`Thank you for your feedback!`)
+    const messageText =
+        `Emotion: ${emotion}\nMessage: ${note}`
+    sendMessage(token, telegramUserId, messageText).then(
+        () => response.status(200).send(`Thank you for your feedback!`))
 }
